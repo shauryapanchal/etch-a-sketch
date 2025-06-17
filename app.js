f4ea19a927fba04fefeca3c18333a4c1e597489e
Shaurya Panchal <122795698+shauryapanchal@users.noreply.github.com>
2025-06-17 13:05:02 +0000

    Add the main logic for each sketchpad button
    added updated html css and javascript
    implemented logic grid size and for the following modes: color, reset, light-shade and dark-shade.

---
+++ app.js
@@ -0,0 +1,168 @@
+const settingMenu = document.querySelector(".settings");
+const DEFAULT_COLOR ='rgb(0, 0, 0)';
+const DEFAULT_SIZE = 16;
+const DEFAULT_MODE='color';
+let color=DEFAULT_COLOR;
+let size=DEFAULT_SIZE;
+let mode=DEFAULT_MODE;
+settingMenu.addEventListener('click',(event)=>{
+    if(event.target.tagName ==='INPUT'){
+        event.target.addEventListener("change", ()=>{
+            setColor();
+        });
+    }
+    else{
+        setParameters(event.target.innerText);
+    }
+});
+
+function setParameters(buttonName){
+    switch(buttonName){
+        case "Reset":
+            clear();
+            gridDraw();
+            break;
+        case "Grid Size":
+            gridSizeInput();
+            break;
+        case "Color":
+            mode='color';
+            break;
+        case "Rainbow":
+            mode='rainbow';
+            break;
+        case "Shade-Light":
+            mode='shade light'
+            break;
+        case "Shade-Dark":
+            mode='shade dark'
+            break;
+    }
+}
+
+function gridSizeInput(){
+    clear();
+    size=+prompt("Enter the grid size: (should be less than 100)");
+    gridDraw();
+}
+
+function setColor(){
+    const colorInput=document.querySelector("input");
+    color=colorInput.value;
+}
+
+function lighten(currentColor){
+    console.log(currentColor);
+    currentColor=currentColor.substring(3);
+    console.log(currentColor);
+    currentColor=currentColor.substring(1,currentColor.length-1);
+    console.log(currentColor);
+    currentColor=currentColor.split(", ");
+    console.log(currentColor);
+    let r=+currentColor[0];
+    let g=+currentColor[1];
+    let b=+currentColor[2];
+
+    r+=25.5
+    g+=25.5;
+    b+=25.5;
+    if(r>=255){
+        r=255;
+    }
+    if(g>=255){
+        g=255;
+    }
+    if(b>=255){
+        b=255;
+    }
+    let newColor=`rgb(${r}, ${g}, ${b})`;
+    return newColor;
+}
+
+function darken(currentColor){
+    console.log(currentColor);
+    currentColor=currentColor.substring(3);
+    console.log(currentColor);
+    currentColor=currentColor.substring(1,currentColor.length-1);
+    console.log(currentColor);
+    currentColor=currentColor.split(", ");
+    console.log(currentColor);
+    let r=+currentColor[0];
+    let g=+currentColor[1];
+    let b=+currentColor[2];
+
+    r-=25.5
+    g-=25.5;
+    b-=25.5;
+    if(r<=0){
+        r=0;
+    }
+    if(g<=0){
+        g=0;
+    }
+    if(b<=0){
+        b=0;
+    }
+    let newColor=`rgb(${r}, ${g}, ${b})`;
+    return newColor;
+}
+
+function renderLogic(){
+    const colBoxes=document.querySelectorAll(".columns");
+    let isDrawing=false;
+    colBoxes.forEach(element => {
+        element.addEventListener('mousedown', (e)=>{
+            e.preventDefault();
+            isDrawing=true;
+        })
+        element.addEventListener('mouseup', ()=>{
+            isDrawing=false;
+        })
+        element.addEventListener('mouseenter', ()=>{
+            if(isDrawing){
+                if(mode==='color'){
+                    element.style.backgroundColor=color;
+                }
+                else if(mode==='rainbow'){
+                    let r=Math.floor(255*Math.random());
+                    let g=Math.floor(255*Math.random());
+                    let b=Math.floor(255*Math.random());
+                    element.style.backgroundColor=`rgb(${r},${g},${b})`;
+                }
+                else if(mode==='shade light'){
+                    let currentColor=element.style.backgroundColor;
+                    element.style.backgroundColor=lighten(currentColor);
+                }
+                else{
+                    let currentColor=element.style.backgroundColor;
+                    element.style.backgroundColor=darken(currentColor);
+                }
+            }
+        })
+    });
+}
+
+function clear(){
+    const sketchPad=document.querySelector(".sketch-pad");
+    const rowBoxes=document.querySelectorAll(".rows");
+    rowBoxes.forEach(element => {
+        sketchPad.removeChild(element);
+    });
+}
+
+function gridDraw(){
+    const sketchPad=document.querySelector(".sketch-pad");
+    for(let i=0; i<size; i++){
+        let rowBox=document.createElement("div");
+        rowBox.classList.add("rows");
+        for(let j=0; j<size; j++){
+            let colBox=document.createElement("div");
+            colBox.classList.add("columns");
+            rowBox.appendChild(colBox);
+        }
+        sketchPad.appendChild(rowBox);
+    }
+    renderLogic();
+}
+
+gridDraw();
